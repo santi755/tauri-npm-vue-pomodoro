772c68a13f9bdcc4303ffc66433b2940513eb793
Santiago Mansilla Labbé <santi755@gmail.com>
2024-01-12 23:49:01 +0100

    Add Tag component and update Home page

---
+++ src/components/atoms/Tag.vue
@@ -0,0 +1,34 @@
+<template>
+  <span
+    class="inline-block bg-green-800 rounded-full px-3 py-1 text-sm font-regular text-white"
+  >
+    <span
+      class="inline-block bg-red-500 rounded-full w-2 h-2 mr-1"
+      :class="tagTypeColor[props.tagType]"
+    ></span>
+    <span> {{ props.tag }}</span>
+  </span>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  tag: {
+    type: String,
+    required: true,
+  },
+  tagType: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const tagTypeColor = {
+  1: "bg-red-500",
+  2: "bg-green-500",
+  3: "bg-blue-500",
+  4: "bg-yellow-500",
+  5: "bg-indigo-500",
+  6: "bg-purple-500",
+  7: "bg-pink-500",
+};
+</script>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="h-screen w-screen bg-green-900 flex items-center justify-center">
-    <h1 class="">Welcome to Tauri!!</h1>
-    <Greet />
+  <div class="h-screen w-screen bg-green-900 p-8">
+    <h1 class="w-full text-white text-center mb-10">
+      ¡Empieza el Pomodoro Time!
+    </h1>
 
-    <Button />
+    <!-- <Greet /> -->
+
+    <div class="w-full text-center mb-6">
+      <Tag class="" tag="Vue" tagType="3" />
+    </div>
+    <div class="w-full text-center">
+      <Button />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Button from "../../components/atoms/Button.vue";
+import Tag from "../../components/atoms/Tag.vue";
 import Greet from "../../components/Greet.vue";
 </script>
