f28e8841f0190fbc500d85e181fe35123c5b8067
Santiago Mansilla Labbé <santi755@gmail.com>
2024-01-14 16:26:27 +0100

    Refactor countdown service

--- src/composables/useCountdownService.ts
+++ src/composables/useCountdownService.ts
@@ -1,30 +1,33 @@
 export const useCountdownService = () => {
-  let countdownTimer: NodeJS.Timeout;
+    const COUNTDOWN_LIMIT_IN_SECONDS = 0;
+    const SECONDS_IN_MINUTE = 60;
+    const SECONDS_TO_FORMAT = 10;
+    let countdownTimer: NodeJS.Timeout;
 
-  const startCountdown = (countdownRestInSeconds: number, updateCallback: (value: number) => void) => {
-    countdownTimer = setInterval(() => {
-      countdownRestInSeconds--;
-      updateCallback(countdownRestInSeconds);
-      if (countdownRestInSeconds === 0) {
+    const startCountdown = (countdownRestInSeconds: number, updateCallback: (value: number) => void) => {
+        countdownTimer = setInterval(() => {
+            countdownRestInSeconds--;
+            updateCallback(countdownRestInSeconds);
+            if (countdownRestInSeconds === COUNTDOWN_LIMIT_IN_SECONDS) {
+                clearInterval(countdownTimer);
+            }
+        }, 1000);
+    };
+
+    const formatToMinutesAndSeconds = (countdownRestInSeconds: number) => {
+        const minutes = Math.floor(countdownRestInSeconds / SECONDS_IN_MINUTE);
+        const seconds = countdownRestInSeconds - minutes * SECONDS_IN_MINUTE;
+        const formattedSeconds = seconds < SECONDS_TO_FORMAT ? `0${seconds}` : seconds;
+        return `${minutes}:${formattedSeconds}`;
+    };
+
+    const stopCountdown = () => {
         clearInterval(countdownTimer);
-      }
-    }, 1000);
-  };
+    };
 
-  const formatToMinutesAndSeconds = (countdownRestInSeconds: number) => {
-    const minutes = Math.floor(countdownRestInSeconds / 60);
-    const seconds = countdownRestInSeconds - minutes * 60;
-    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-    return `${minutes}:${formattedSeconds}`;
-  };
-
-  const stopCountdown = () => {
-    clearInterval(countdownTimer);
-  }
-
-  return {
-    formatToMinutesAndSeconds,
-    startCountdown,
-    stopCountdown
-  };
+    return {
+        formatToMinutesAndSeconds,
+        startCountdown,
+        stopCountdown,
+    };
 };
